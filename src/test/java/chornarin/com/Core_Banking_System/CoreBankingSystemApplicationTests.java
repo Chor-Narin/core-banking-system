e53a43548d72eaa99f89247c1445e6b1ca95b829
Chor Narin <naritkh178@gmail.com>
2026-01-30 16:24:53 +0700

    initail core banking system

---
+++ src/test/java/chornarin/com/Core_Banking_System/CoreBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package chornarin.com.Core_Banking_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreBankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
